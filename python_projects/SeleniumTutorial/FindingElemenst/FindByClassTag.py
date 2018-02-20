a3763c9e689c193016f90414b27439e8364ac167
lunchesque <lunchesque1993@gmail.com>
2018-02-20 15:11:10 +0300

    inspecting elements

--- python_projects/SeleniumTutorial/FindingElemenst/FindByClassTag.py
+++ python_projects/SeleniumTutorial/FindingElemenst/FindByClassTag.py
@@ -6,17 +6,17 @@ class FindByLinkText():
         driver = webdriver.Chrome()
         driver.maximize_window()
         driver.get(baseUrl)
-        elementByClassName = driver.find_element_by_class_name("inputs")
+        elementByClassName = driver.find_element_by_class_name("displayed-class")
         elementByClassName.send_keys("Test")
 
         if elementByClassName is not None:
             print("We found an element by Class Name")
 
-        elementByTagName = driver.find_element_by_tag_name("a")
-        elementByTagName.send_keys("Test")
+        elementByTagName = driver.find_element_by_tag_name("h1")
+        text = elementByTagName.text
 
         if elementByTagName is not  None:
-            print("We found an element by Tag Name")
+            print("We found an element by Tag Name and the text of the element is: " + text)
 
 ff = FindByLinkText()
 ff.test()
