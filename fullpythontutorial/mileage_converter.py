d214ae281f51be11a33279204b671279356d26ca
lunchesque <lunchesque1993@gmail.com>
2018-03-20 13:14:20 +0300

    variables!

---
+++ fullpythontutorial/mileage_converter.py
@@ -0,0 +1,3 @@
+kms = float(input("How many kilometers did you cycle today?\n"))
+print("Ok, you said: " + str(kms) + " kilometers\nIt is "
+                    + str(round(kms / 1.60934, 3)) + " miles")
---
+++ fullpythontutorial/variables.py
@@ -0,0 +1,16 @@
+# cash = 19867324678987.99
+# print(cash / 5)
+# new_line = "hi\nthere"
+# print(new_line)
+# string = "this a back slash: \\"
+# print(string)
+# character_del = "hel\blo"
+# print(character_del)
+# mountains = "/s\\t/r\\i/n\\"
+# print(mountains)
+# x = 10
+# print("sdfjsfsdsf {} dslfkjsdlkf".format(x))
+#
+name = "name"
+print(name[0])
+print(name[3])
